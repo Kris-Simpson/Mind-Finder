f6b1e4f261bc22e00193d157fa08613e0eb79c90
Kris-Simpson <the.same.kris@gmail.com>
2013-04-07 20:10:49 +0300

    some editions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url
     else
-      redirect_to root_url, :alert => t(:alert_email_pass_invalid)
+      redirect_to root_url, :alert => t(:alert_email_or_pass_invalid)
     end  
   end
   
