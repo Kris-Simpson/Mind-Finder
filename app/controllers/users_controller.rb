9155fbc1870a94ccb4c96af852a54bd4c87cf445
Kris-Simpson <the.same.kris@gmail.com>
2013-04-08 12:25:32 +0300

    add select tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      UserMailer.welcome_email(@user).deliver
+ #     UserMailer.welcome_email(@user).deliver
       
       redirect_to root_url, :notice => t(:notice_signup_success)
     else
