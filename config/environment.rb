fe8963313751f828dcbfca9bd3a839e6a7bcdc6e
Kris-Simpson <the.same.kris@gmail.com>
2013-04-14 16:02:26 +0300

    configured email sending

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,18 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+MindFinder::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "",
+    password: ""
+  }
+end
+
 # Initialize the rails application
 MindFinder::Application.initialize!
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,18 +68,7 @@ MindFinder::Application.configure do
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'mind-finder.herokuapp.com' }
 
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
-
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "mind-finder.herokuapp.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "mind.finder.app@gmail.com",
-    password: "djdouble"
-  }
 end
