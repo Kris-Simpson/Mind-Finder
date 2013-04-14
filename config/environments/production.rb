e536dfcb2941c53c5129acd685260f78ef7e083c
Kris-Simpson <the.same.kris@gmail.com>
2013-04-14 15:39:03 +0300

    configure email sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,22 @@ MindFinder::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'mind-finder.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "mind-finder.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: mind.finder.app@gmail.com,
+    password: djdouble
+  }
 end
