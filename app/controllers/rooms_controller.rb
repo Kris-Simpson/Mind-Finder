c16dc8ace362735aa0528daee0a443dbc3ef77bd
Kris-Simpson <the.same.kris@gmail.com>
2013-04-22 00:05:02 +0300

    some troubleshooting

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,6 @@
 class RoomsController < ApplicationController
   def index
-    @rooms = Room.where(:user_id => current_user.id)
+  #  @rooms = Room.where(:user_id => current_user.id)
   end
 
   def show
